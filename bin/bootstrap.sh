bbeadcb58b6d4eb99c30fcf8e8311c57e9647a27
Lance W <50839441+disposableraft@users.noreply.github.com>
2020-12-02 16:28:13 -0800

    Misc updates

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -8,3 +8,15 @@ for file in $files
     do
         ln -fs "$workingdir/data/$file" "$HOME/.$file"
 done
+
+# Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew update
+
+# Install postgres and mysql
+brew install postgres
+brew install mysql
+
+# Install Python
+brew install python
